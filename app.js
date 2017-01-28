8f1ca4db0cf0c27e3c70d1baf6abee25525e93ff
Limon Monte <limon.monte@gmail.com>
2017-01-28 20:23:44 +0200

    Make it work :)

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-ymaps.ready(function () {
+ymaps.ready(init);
+
+function init () {
   var myMap = new ymaps.Map('map', {
-    center: [55.751574, 37.573856],
-    zoom: 9
-  }, {
-    searchControlProvider: 'yandex#search'
+    center: [55.76, 37.64],
+    zoom: 7
   });
 
-  var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-    hintContent: 'Собственный значок метки',
-    balloonContent: 'Это красивая метка'
-  }, {
-    iconLayout: 'default#image',
-    iconImageHref: 'images/myIcon.gif',
-    iconImageSize: [30, 42],
-    iconImageOffset: [-5, -38]
-  });
+  $.getJSON('objects.json', function(json) {
+    json.forEach(function(object) {
 
-  myMap.geoObjects.add(myPlacemark);
-});
+      console.log(object.address)
+      $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=' + object.address + '&key=AIzaSyCRqyXLK2y1iYyFoGjqIFHPRzDZe85W-Tk', function(response) {
+        if (response.results.length) {
+          var location = response.results[0].geometry.location
+          myMap.geoObjects.add(
+            new ymaps.Placemark([location.lat, location.lng],
+              {
+                iconContent: object.title,
+                balloonContent: object.content
+              },
+              {preset: 'islands#' + (object.color? object.color : 'green') + 'StretchyIcon'}
+            )
+          );
+        } else {
+          swal({
+            type: 'error',
+            title: 'Непонятный адрес',
+            html: object.address
+          });
+        }
+      });
+    });
+  }).fail(function(error) {
+    swal({
+      type: 'error',
+      title: 'Ошибка в objects.json',
+      html: 'Воспользуйся <a href="https://jsonformatter.curiousconcept.com/" target="_blank">https://jsonformatter.curiousconcept.com/</a> для поиска ошибки в файле'
+    });
+  })
+}
